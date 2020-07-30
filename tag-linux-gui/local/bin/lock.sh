099e4c24e69f994bd9d587344f08e945dbd5ae03
Karl Hole Totland <karl@totland.dev>
2020-07-30 23:21:28 +0200

    i3lock: use xss-lock to trigger locking on events

--- tag-linux-gui/local/bin/lock.sh
+++ tag-linux-gui/local/bin/lock.sh
@@ -2,9 +2,17 @@
 
 # A wrapper script for i3lock-color to customise look and feel
 
+set -e
+xset s off dpms 0 10 0
+
 i3lock \
 	--screen 1 \
 	--blur 6 \
 	--clock \
 	--datestr="%a, %-d. %b %Y" \
-	--pass-media-keys
+	--pass-media-keys \
+	--ignore-empty-password \
+	--show-failed-attempts \
+	--nofork
+
+xset s off -dpms
