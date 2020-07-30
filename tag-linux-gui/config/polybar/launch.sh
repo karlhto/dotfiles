872e3f37f52f1654cf1cc5d34060a18f4738a8ba
Karl Hole Totland <karl@totland.dev>
2020-07-30 19:35:18 +0200

    polybar: laptop tweaks

--- tag-linux-gui/config/polybar/launch.sh
+++ tag-linux-gui/config/polybar/launch.sh
@@ -3,11 +3,18 @@
 # Terminate already running bar instances
 killall -q polybar
 
+HOSTNAME=$(hostname)
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar desktop-left &
-polybar desktop-right &
+if [ $HOSTNAME == 'krrl' ]; then
+    polybar desktop-left &
+    polybar desktop-right &
+else
+    polybar laptop &
+fi
+
 
 echo "Polybar launched..."
